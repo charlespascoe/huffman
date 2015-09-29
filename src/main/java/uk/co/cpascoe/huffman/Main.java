ad05ff732ea15e0323562050569db3bd43e5c2f4
Charles Pascoe <cpascoe95@gmail.com>
2015-09-29 16:39:57 +0100

    Added simple test code

--- src/main/java/uk/co/cpascoe/huffman/Main.java
+++ src/main/java/uk/co/cpascoe/huffman/Main.java
@@ -6,5 +6,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Huffman!");
+
+        HuffmanCompressor h = new HuffmanCompressor();
+
+        for (byte b : h.compress(new byte[] {100, 50, 100, 100, 0, 50})) {
+            System.out.println(b);
+        }
     }
 }
