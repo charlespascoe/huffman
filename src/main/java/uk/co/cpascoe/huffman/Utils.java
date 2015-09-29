a0a934fbcc9e17a8083e73c4ba5778ab5b638a07
Charles Pascoe <cpascoe95@gmail.com>
2015-09-29 19:28:46 +0100

    Added Utils.byteFromBits

--- src/main/java/uk/co/cpascoe/huffman/Utils.java
+++ src/main/java/uk/co/cpascoe/huffman/Utils.java
@@ -14,9 +14,24 @@ public static class Utils {
 
         for (int i = 7; i >= 0; i--) {
             bits[i] = (byte)((val & mask) > 0 ? 1 : 0);
-            mask = mask >> 1;
+            mask = mask << 1;
         }
 
         return bits;
+    }
+
+    public byte byteFromBits(byte[] bits) {
+        // TODO: Check bits length
+
+        byte val;
+
+        byte bitValue = (byte)128;
+
+        for (byte b : bits) {
+            val += b * bitValue;
+            bitValue = bitValue >> 1;
+        }
+
+        return val;
     }
 }
